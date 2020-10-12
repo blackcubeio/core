d7a688d9cbbc67d22d378868689be1f8cc7ffd5f
Philippe Gaultier <pgaultier@ibitux.com>
2020-10-12 08:25:52 +0200

    Prepare plugins structure

---
+++ src/interfaces/PluginBootstrapInterface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * PluginBootstrapInterface.php
+ *
+ * PHP version 7.2+
+ *
+ * @author Philippe Gaultier <pgaultier@redcat.io>
+ * @copyright 2010-2020 Redcat
+ * @license https://www.redcat.io/license license
+ * @version XXX
+ * @link https://www.redcat.io
+ * @package blackcube\core\interfaces
+ */
+
+namespace blackcube\core\interfaces;
+
+use Yii;
+use yii\base\Model;
+use yii\base\Application;
+
+/**
+ * Interface PluginBootstrapInterface
+ *
+ * @author Philippe Gaultier <pgaultier@redcat.io>
+ * @copyright 2010-2020 Redcat
+ * @license https://www.redcat.io/license license
+ * @version XXX
+ * @link https://www.redcat.io
+ * @package blackcube\core\interfaces
+ */
+interface PluginBootstrapInterface {
+
+    /**
+     * @param string $moduleUid Core module uniqueId
+     * @param Application $app
+     */
+    public function bootstrapCore($moduleUid, $app);
+
+}
