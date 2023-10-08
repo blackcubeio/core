5513a43ade0bc2388ebcaefc4008a417cbdbbeac
Philippe Gaultier <pgaultier@redcat.io>
2023-10-08 19:35:34 +0200

    Release 3.0.5

--- src/Module.php
+++ src/Module.php
@@ -177,7 +177,7 @@ class Module extends BaseModule implements BootstrapInterface
     /**
      * @var string version number
      */
-    public $version = 'v3.0-dev';
+    public $version = 'v3.0.5';
     /**
      * {@inheritDoc}
      */
