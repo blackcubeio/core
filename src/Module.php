0e00f5f7ed4ef6bfd5e27667348b712849b71cd7
Philippe Gaultier <pgaultier@redcat.io>
2023-11-07 09:39:11 +0100

    Merge back

--- src/Module.php
+++ src/Module.php
@@ -210,7 +210,7 @@ class Module extends BaseModule implements BootstrapInterface
     /**
      * @var string version number
      */
-    public $version = 'v3.1.3';
+    public $version = 'v3.1-dev';
 
     /**
      * {@inheritDoc}
