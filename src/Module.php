4b6a55c9a7a4201709cdcc331d63e788e67bfb14
Philippe Gaultier <pgaultier@gmail.com>
2025-07-17 11:13:34 +0200

    merge back

--- src/Module.php
+++ src/Module.php
@@ -222,7 +222,7 @@ class Module extends BaseModule implements BootstrapInterface
     /**
      * @var string version number
      */
-    public $version = 'v3.6.0';
+    public $version = 'v3.x-dev';
 
     /**
      * {@inheritDoc}
